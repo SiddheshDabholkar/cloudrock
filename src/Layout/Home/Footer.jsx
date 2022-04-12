c2ca1754127e39e442be6795e739095d0cbcc09a
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-04-13 04:54:52 +0530

    footer

--- src/Layout/Home/Footer.jsx
+++ src/Layout/Home/Footer.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Footer.scss";
 
 export default function Footer() {
-  return <footer className="Footer">Footer</footer>;
+  return (
+    <footer className="Footer">
+      <span>&copy; All rights reserved</span>
+    </footer>
+  );
 }
