4d0e7e2dc99b040f929e2a90bdc20bec47abd073
Siddhesh Dabholkar <iamsiddhesh22@gmail.com>
2022-04-12 20:14:42 +0530

    tips card

--- src/Layout/Home/pages/Dashboard/tips/Tips.jsx
+++ src/Layout/Home/pages/Dashboard/tips/Tips.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import "./Tips.scss";
 
 export default function Tips() {
-  return <div className="Tips">Tips</div>;
+  return (
+    <div className="Tips">
+      <h1 className="TipsHead">Tips</h1>
+      <div className="TipsCard">
+        <h1 className="TipsCardTop">Gold is up 20%</h1>
+        <div className="TipsCardBottom">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          pellentesque, lacus non congue convallis, dolor elit cursus urna, ac
+          feugiat neque elit id risus. Sed dolor eros, ullamcorper nec libero
+          vitae, malesuada fringilla eros. Sed ultrices, mauris ut porta cursus,
+          mauris dolor vulputate dolor, sed accumsan purus eros non sem.
+          Pellentesque nec mi nisl. Cras rhoncus ac elit a varius. In urna nunc,
+          scelerisque vel enim malesuada, varius ullamcorper quam. Etiam rhoncus
+          fringilla odio sed fermentum.
+        </div>
+      </div>
+    </div>
+  );
 }
